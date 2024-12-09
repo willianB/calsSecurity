9affee15edbe250e1da68df3b8634aa689e104db
willianB <wilianandres1@gmail.com>
2024-12-09 10:27:00 -0500

    First commit security lib

---
+++ app/src/main/java/com/zefiroft/calsecurity/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.zefiroft.calsecurity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zefiroft.calsecurity.ui.theme.CalSecurityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalSecurityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CalSecurityTheme {
+        Greeting("Android")
+    }
+}
---
+++ calSecurity/src/main/java/com/zefiroft/calsecurity/Encrypt.kt
@@ -0,0 +1,8 @@
+package com.zefiroft.calsecurity
+
+object Encrypt {
+
+    fun encryptString(data: String): String {
+        return data.reversed()
+    }
+}
